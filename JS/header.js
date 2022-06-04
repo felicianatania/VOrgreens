1b0728170096b074c78ae81d4f7ed90f49bfe1d4
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2022-06-04 16:05:38 +0700

    responsive update

--- JS/header.js
+++ JS/header.js
@@ -1,8 +1,13 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.nav-links');
+let xbtn = document.querySelector('.close');
 
 menu.onclick = () => {
     navbar.classList.toggle('active');
+}
+
+xbtn.onclick = () => {
+    navbar.classList.remove('active');
 }
 
 window.onscroll = () => {
